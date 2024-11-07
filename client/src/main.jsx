1eb73d09aa24e44871858fc02798aee5fe91bb12
Keller471 <maxixgil471@gmail.com>
2024-11-07 19:25:21 -0300

    Menu init

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/home.jsx'
+import Menu from './pages/Menu/menu.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Menu/>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
