132dbff6a4638fe8e576f4e3c63e0d905541c4f5
Keller471 <maxixgil471@gmail.com>
2024-11-17 07:42:57 -0300

    Menu changes proximamente categorias

--- client/src/pages/Menu/menu.jsx
+++ client/src/pages/Menu/menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react';
+import styles from './menu.module.css'
 const Menu = () => {
     const [pg, setpg] = useState(1);
     const [platos, setplatos] = useState()
@@ -22,30 +23,45 @@ const Menu = () => {
 
     return (
         <>
-            <header>
-                <div className="Titulo"></div>
-            </header>
-            <div className="menu">
-                <div className="categorias">
+            <div className={styles.container}>
+      <div className={styles.sidebar}>
+        <h2>Categorías</h2>
+        <ul className={styles.categories}>
+          <li><a href="#" className={styles.category} data-category="pizzas">Pizzas</a></li>
+          <li><a href="#" className={styles.category} data-category="hamburguesas">Hamburguesas</a></li>
+          <li><a href="#" className={styles.category} data-category="ensaladas">Ensaladas</a></li>
+          <li><a href="#" className={styles.category} data-category="tacos">Tacos</a></li>
+          <li><a href="#" className={styles.category} data-category="pollo">Pollo</a></li>
+          <li><a href="#" className={styles.category} data-category="sopas">Sopas</a></li>
+          <li><a href="#" className={styles.category} data-category="pasta">Pasta</a></li>
+          <li><a href="#" className={styles.category} data-category="postres">Postres</a></li>
+        </ul>
+      </div>
 
-                </div>
-                <div className="platos">
-                    {platos ? (
-                        platos.map((plato) => (
+      <div className={styles.mainContent}>
+        <div className={styles.bienvenida}>Bienvenido al Menú</div>
+        <div id="menu-items" className={styles.menuItems}>
+          <div className={styles.platos}>
+            {platos ? (
+              platos.map((plato) => (
+                <a href="product" className={styles.card} key={plato.id}>
+                  <div className={styles.nombre}>{plato.nombre_Plato}</div>
+                  <div className={styles.desc}>{plato.descripcion_Plato}</div>
+                  <div className={styles.precio}>{plato.precio}</div>
+                  <div className={styles.im}>
+                    <img src={plato.foto_Url} alt={plato.nombre_Plato} />
+                  </div>
+                </a>
+              ))
+            ) : (
+              <div className={styles.loading}>Cargando...</div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
 
-                            <a href="product" className="card" key={plato.id}>
-                                <div className="nombre">{plato.nombre_Plato}</div>
-                                <div className="desc">{plato.descripcion_Plato}</div>
-                                <div className="precio">{plato.precio}</div>
-                                <div className="im">{plato.foto_Url}</div>
-                            </a>
 
-                        ))
-                    ) : (
-                        <div>cargando</div>
-                    )}
-                </div>
-            </div>
         </>
     )
 }
